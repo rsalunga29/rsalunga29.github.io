96199647bb756268bed2cf5b8209b36e3b40207a
Akio <rsalunga29@gmail.com>
2018-11-13 17:57:32 +0800

    New information

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -2,8 +2,8 @@ var my_info = new Vue({
 	el: "#my_info",
 	data: {
 		full_name: "Roland Emmanuel (Akio) Salunga",
-		occupation: "Software Engineer",
-		company: "OrangeApps Inc.",
-		location: "Manila, Philippines"
+		occupation: "Data Analyst",
+		company: "Paydro",
+		location: "Quezon City, Philippines"
 	}
 })
